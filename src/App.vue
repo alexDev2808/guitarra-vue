fec0885b133d3048bf3618b98c7438afafe030d6
J. Alexis TH <colores.2808.8000@gmail.com>
2023-08-25 18:02:00 -0600

    Vaciar el carrito

--- src/App.vue
+++ src/App.vue
@@ -46,6 +46,10 @@ const eliminarProducto = (id) => {
   carrito.value = carrito.value.filter(producto => producto.id !== id)
 }
 
+const vaciarCarrito = () => {
+  carrito.value = [];
+}
+
 </script>
 
 <template>
@@ -57,6 +61,7 @@ const eliminarProducto = (id) => {
     @incrementar-cantidad="incrementarCantidad"
     @agregar-carrito="agregarCarrito"
     @eliminar-producto="eliminarProducto"
+    @vaciar-carrito="vaciarCarrito"
   />
 
   <main class="container-xl mt-5">
